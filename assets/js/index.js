93028e3d3737b8aefb0e821d4c688adfb828baf2
Davi Oliveira <davioliveira.java@gmail.com>
2020-05-05 16:06:46 -0300

    Creating new features and adding a logo!

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,16 @@
-const textarea = document.getElementById('resumo')
-const resultado = document.getElementById('resultado')
+const textarea = document.getElementById('text')
+const resume = document.getElementById('resume')
+const clearButton = document.getElementById('clear-btn')
+const copyButton = document.getElementById('copy-btn')
+const loadingDiv = document.getElementById('loading-div')
 
-
-async function resumeText(){
-    if(textarea.value == null || textarea.value === '') {
-        alert('Digite seu texto!')
-            return
-        }
-
-        await Algorithmia.client("sim1yuhC9YvgFoljQ18M5QtoDio1")
-        .algo("nlp/Summarizer/0.1.8")
-        .pipe(textarea.value)
-        .then(function(output){ 
-            resultado.value = output.result
-            showText()
-        })
-    }
-
-function showText() {
-    resultado.style.display = 'block'
-}
 
 window.onload = () => {
-    const title = 'Uma ajudinha pra você! ;)'
-    const content =  'Caso o texto seja muito longo NÃO cole ele todo! COLE A CADA DOIS PARÁGRAFOS! Pois resultará num resumo com ainda mais qualidade!'
-    materialAlert(title, content, function(result){
-        if(result==true) console.log('OK button pressed');
-        else console.log('No button was pressed');
-    })
-
+    const title = 'Uma ajudinha pra você!'
+    const content =  'Caso o texto seja muito longo NÃO cole todo! COLE A CADA TRÊS PARÁGRAFOS. Assim resultará num resumo melhor!'
+    materialAlert(title, content)
 }
+
 
 function darkMode() {
     let button = document.querySelector('#dark-mode-btn')
@@ -47,4 +28,53 @@ function darkMode() {
         button.value = 'off'
     }
 
+}
+
+function resumeText(){
+    if(textarea.value == null || textarea.value === '') {
+        alert('DIGITE ou COLE seu texto!')
+        return
+    }
+
+    showLoading(true)
+
+    Algorithmia.client("sim1yuhC9YvgFoljQ18M5QtoDio1")
+        .algo("nlp/Summarizer/0.1.8")
+        .pipe(textarea.value)
+        .then(function(output){ 
+            resume.value = output.result
+            showText()
+            showLoading(false)
+        })
+}
+
+function showClearButton(isHidden) {
+    if(!isHidden) {
+        return
+    }
+    clearButton.style.display = 'block'
+}
+
+
+function showText() {
+    resume.style.display = 'block'
+    copyButton.style.display = 'block'
+}
+
+function showLoading(isLoading) {
+    isLoading ? loadingDiv.style.display = 'block' : loadingDiv.style.display = 'none'
+}
+
+function copyText() {
+    resume.select()
+    try {
+        const successful = document.execCommand('copy')
+        var msg = successful ? alert('Texto copiado!') : alert('Tente novamente!')
+    } catch (err) {
+        alert('Opa, Não conseguimos copiar o texto, é possivel que o seu navegador não tenha suporte, tente usar Crtl+C.')
+    }
+}
+
+function clearText() {
+    textarea.value = ''
 }
